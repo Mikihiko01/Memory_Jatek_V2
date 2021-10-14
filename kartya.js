c4b7e2ffa1ba594477b0c86c5e665c85fadfa336
T.Ádám <78810601+Mikihiko01@users.noreply.github.com>
2021-10-14 11:19:30 +0200

    Add files via upload

---
+++ kartya.js
@@ -0,0 +1,48 @@
+class Kartya {
+    constructor(faljnev, elem) {
+        this.elem = elem;//aktuális div elem
+        this.faljnev = faljnev;
+        //console.log(faljnev);
+        //az img elem ,akkor elem.atrr("src", faljnev);
+        this.kepElem = elem.children("img");
+        //az aktuális div elemunk kép eleme lesz 
+        this.kepElem.attr("src", this.faljnev);
+
+        this.allapot = false;//kezdetben a háttere látszik
+        this.hatter = "kepek/hatter.jpg";
+        this.setLap();
+        //console.log(this);
+        this.elem.on("click", () => {
+            
+            this.atvalt();
+            this.KattintasTrigger();//ezzel váltom ki az eseményt
+
+        });
+
+
+    }
+
+    atvalt(){
+        this.allapot=!this.allapot;
+        this.setLap();
+    }
+
+    setLap() {
+        if (this.allapot) {
+            this.kepElem.attr("src", this.faljnev);
+        } else {
+            this.kepElem.attr("src", this.hatter);
+        }
+    }
+    KattintasTrigger(){
+        //let esemeny = new Event("kartyaKattintas");
+        let esemeny = new CustomEvent("kartyaKattintas",{detail:this});
+        //úgy hozzunk létre eseményt hogy azt is meg mondjuk hogy melyik objektum váltottaki
+       console.log("esemény kiváltva");
+        window.dispatchEvent(esemeny);
+    }
+
+    eletuntet(){
+        this.elem.css("visibility","hidden");
+    }
+}
---
+++ scriptJatek.js
@@ -0,0 +1,51 @@
+$(function () {
+    const szuloElem = $("article");
+    const sablonElem = $(".karta");
+    const meret = 3;
+
+    const kivalasztottKartyaTomb = [];
+
+    for (let index = 0; index < meret; index++) {
+        for (let k = 0; k < 2; k++) {
+            const ujElem = sablonElem.clone().appendTo(szuloElem);
+            const kartya = new Kartya("kepek/kep" + (index + 1) + ".jpg", ujElem);
+
+        }
+
+
+    }
+
+    sablonElem.remove();
+
+    //itt tudjuk számolni hogy hány kártya van felforditva
+    $(window).on("kartyaKattintas", (event) => {
+        console.log(event.detail);//aktuális kártya adata
+
+        //az,amelyik kiválasztotta az eseményt.
+        kivalasztottKartyaTomb.push(event.detail);
+
+        //akkor van két kártya felforditva ha tömb hossza 2
+        if (kivalasztottKartyaTomb.length = 2) {
+            if (kivalasztottKartyaTomb[0].faljnev == kivalasztottKartyaTomb[1].faljnev) {
+                console.log("egyforma");
+                //eltüntetni a kártyákat
+                setTimeout(function () {
+                    kivalasztottKartyaTomb[0].eletuntet();
+                    kivalasztottKartyaTomb[1].eletuntet();
+
+                    kivalasztottKartyaTomb[0].atvalt();
+                    kivalasztottKartyaTomb[1].atvalt();
+                    kivalasztottKartyaTomb.splice(0, 2);
+
+                }, 1000);
+
+            } else {
+                console.log(" NEM egyforma");
+                kivalasztottKartyaTomb.splice(0, 2);
+
+            }
+            
+        }
+    });
+
+});
